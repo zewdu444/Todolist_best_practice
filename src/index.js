9fe80c86beec57b6797341df08c10595cc695efd
zewdu erkyhun <zewdu444@gmail.com>
2023-01-24 11:32:01 +0300

    Classify into small function

--- src/index.js
+++ src/index.js
@@ -25,68 +25,80 @@ window.addEventListener('load', () => {
   }
   dynamicDisplay();
 });
-
-// insert new task  by click
-insertButton.addEventListener('click', () => {
+// used to store and display tasks
+const updateDisplay = () => {
   createTask(inputTask.value);
   storeTasks(tasks);
   dynamicDisplay();
   inputTask.value = '';
+};
+
+// insert new task  by click
+insertButton.addEventListener('click', () => {
+  updateDisplay();
 });
 // insert new task  pressing enter button
 inputTask.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
-    createTask(inputTask.value);
-    storeTasks(tasks);
-    dynamicDisplay();
-    inputTask.value = '';
+    updateDisplay();
   }
 });
+
+// updating list in on selected list item
+const listUpdate = (e) => {
+  const selectedlist = document.getElementById(e.target.id);
+  const deleteButton = selectedlist.querySelector(`#delete${e.target.id}`);
+  const moveButton = selectedlist.querySelector(`#move${e.target.id}`);
+  const editText = selectedlist.querySelector(`#text${e.target.id}`);
+  const id = Number(e.target.id);
+
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].style.backgroundColor = 'white';
+    deleteItem[i].style.display = 'none';
+    moveItem[i].style.display = 'inline-block';
+  }
+  selectedlist.style.backgroundColor = '#FFFECE';
+  deleteButton.style.display = 'inline-block';
+  moveButton.style.display = 'none';
+  deleteButton.addEventListener('click', () => {
+    deleteTask(id);
+    storeTasks(tasks);
+    dynamicDisplay();
+  });
+  selectedlist.addEventListener('input', () => {
+    updateTask(id, editText.innerHTML);
+  });
+};
+
+// updating input element when on click
+const inputUpdate = (e) => {
+  const id = e.target.id.toString();
+  const index = Number(id.slice(5));
+  const checkCompleted = document.getElementById(e.target.id);
+  const editText = document.querySelector(`#text${index}`);
+  if (checkCompleted.checked === true) {
+    completeTask(index, true);
+    editText.innerHTML = editText.innerHTML.strike();
+    updateTask(index, editText.innerHTML);
+    storeTasks(tasks);
+  } else {
+    completeTask(index, false);
+    editText.innerHTML = editText.innerHTML.replace('<strike>', '');
+    updateTask(index, editText.innerHTML);
+    storeTasks(tasks);
+  }
+};
 
 // delete and edit implementation
 list.addEventListener('click', (e) => {
   if (e.target && e.target.nodeName === 'LI') {
-    const selectedlist = document.getElementById(e.target.id);
-    const deleteButton = selectedlist.querySelector(`#delete${e.target.id}`);
-    const moveButton = selectedlist.querySelector(`#move${e.target.id}`);
-    const editText = selectedlist.querySelector(`#text${e.target.id}`);
-    const id = Number(e.target.id);
     if (e.target.id) {
-      for (let i = 0; i < listItem.length; i += 1) {
-        listItem[i].style.backgroundColor = 'white';
-        deleteItem[i].style.display = 'none';
-        moveItem[i].style.display = 'inline-block';
-      }
-      selectedlist.style.backgroundColor = '#FFFECE';
-      deleteButton.style.display = 'inline-block';
-      moveButton.style.display = 'none';
-      deleteButton.addEventListener('click', () => {
-        deleteTask(id);
-        storeTasks(tasks);
-        dynamicDisplay();
-      });
-      selectedlist.addEventListener('input', () => {
-        updateTask(id, editText.innerHTML);
-      });
+      listUpdate(e);
     }
     storeTasks(tasks);
   } else if (e.target && e.target.nodeName === 'INPUT') {
-    const id = e.target.id.toString();
-    const index = Number(id.slice(5));
-    const checkCompleted = document.getElementById(e.target.id);
-    const editText = document.querySelector(`#text${index}`);
-    if (checkCompleted.checked === true) {
-      completeTask(index, true);
-      editText.innerHTML = editText.innerHTML.strike();
-      updateTask(index, editText.innerHTML);
-      storeTasks(tasks);
-    } else {
-      completeTask(index, false);
-      editText.innerHTML = editText.innerHTML.replace('<strike>', '');
-      updateTask(index, editText.innerHTML);
-      storeTasks(tasks);
-    }
+    inputUpdate(e);
   }
 });
 
